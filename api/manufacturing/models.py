242c53529470f90b753cdddb0cec812f2712f029
CaioNunes <caiofelipe147@gmail.com>
2019-05-25 22:20:42 -0300

    Create Convolutional Neural Network to classify oil images

--- api/manufacturing/models.py
+++ api/manufacturing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os, os.path
 
 # Create your models here.
 class Manufacturing(models.Model):
@@ -10,4 +11,24 @@ class Manufacturing(models.Model):
     actual_ph = models.DecimalField(decimal_places=2, max_digits=4)
     oil_quality = models.CharField(max_length = 10)
     have_fragrance = models.BooleanField()
-    oil_image = models.ImageField() 
+    oil_image = models.ImageField(upload_to='static/oil_images/')
+
+    def reallocate_image(self):
+        image = open('static/oil_images/' + self.oil_image.name, 'rb').read()
+        
+        if(self.oil_quality == 'GOOD'):
+            good_folder = open('dataset/training_oil_dataset/good_oil/good_oil_' + len(os.listdir('dataset/training_oil_dataset/good_oil/')) + '.jpg', 'wb')
+            good_folder.write(image)
+
+        if(self.oil_quality == 'BAD'):
+            good_folder = open('dataset/training_oil_dataset/bad_oil/bad_oil_' + len(os.listdir('dataset/training_oil_dataset/bad_oil/')) + '.jpg', 'wb')
+            good_folder.write(image)
+
+        if(self.oil_quality == 'MEDIUM'):
+            good_folder = open('dataset/training_oil_dataset/medium_oil/medium_oil_' + len(os.listdir('dataset/training_oil_dataset/medium_oil/')) + '.jpg', 'wb')
+            good_folder.write(image)
+
+
+    def save(self, *args, **kwargs):
+        super(Manufacturing, self).save(*args, **kwargs)
+        self.reallocate_image()
