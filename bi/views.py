3d6820bf0a3ab0d4af21328742f8d86b055c6531
Carlos Aragon <carloseraragon@gmail.com>
2019-06-11 21:18:14 -0300

    add method to get soap price

--- bi/views.py
+++ bi/views.py
@@ -14,12 +14,12 @@ def cheaper_soda_americanas():
 
     product_list = soup.select("div.product-grid-item > div:nth-child(1)")
 
-    cheaper_product = {'item_description':'','item_weight':1, 'item_concentration':'', 'item_price':100,'item_link':''}
+    cheaper_product = {'item_description':'','item_volume':1, 'item_concentration':'', 'item_price':100,'item_link':''}
 
     for item in product_list:
         item_name = item.get('name')
         item_concentration = None
-        item_weight = None
+        item_volume = None
         if item.select_one("div.product-grid-item > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > section:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > span:nth-child(1)"):
             item_price = float(re.search(r'R\$\s*(\d{1,5}\,\d{1,2})', item.select_one("div.product-grid-item > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > section:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > span:nth-child(1)").text).group(1).replace(',','.'))
 
@@ -28,19 +28,19 @@ def cheaper_soda_americanas():
 
         if re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name):
             if re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(3) == ("G" or "g"):
-                item_weight = float(re.search(
+                item_volume = float(re.search(
                     r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))/100
             else:
-                item_weight = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))
+                item_volume = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))
             if re.search(r'(\d+)[x|X]', item_name):
-                item_weight = float(re.search(r'(\d+)[x|X]', item_name).group(1)) * item_weight
+                item_volume = float(re.search(r'(\d+)[x|X]', item_name).group(1)) * item_volume
         
         if item.select_one('div.product-grid-item > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)')['href']:
             item_link = "https://www.americanas.com.br" + item.select_one('div.product-grid-item > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)')['href']
 
-        if (item_name and item_price and item_concentration and item_weight) and ((item_price/item_weight) < (cheaper_product['item_price']/cheaper_product['item_weight'])):
+        if (item_name and item_price and item_concentration and item_volume) and ((item_price/item_volume) < (cheaper_product['item_price']/cheaper_product['item_volume'])):
             cheaper_product['item_description'] = item_name
-            cheaper_product['item_weight'] = item_weight
+            cheaper_product['item_volume'] = item_volume
             cheaper_product['item_concentration'] = item_concentration
             cheaper_product['item_price'] = item_price
             cheaper_product['item_link'] = item_link
@@ -54,12 +54,12 @@ def cheaper_soda_ml():
 
     product_list = soup.select("li.results-item")
 
-    cheaper_product = {'item_description':'','item_weight':1, 'item_concentration':'', 'item_price':100,'item_link':''}
+    cheaper_product = {'item_description':'','item_volume':1, 'item_concentration':'', 'item_price':100,'item_link':''}
 
     for item in product_list:
         item_name = item.select_one('span', class_='main-title').text
         item_concentration = None
-        item_weight = None
+        item_volume = None
 
         if item.find(class_='price__decimals'):
             item_price = float(item.find(class_='price__fraction').text + \
@@ -72,18 +72,18 @@ def cheaper_soda_ml():
         
         if re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name):
             if re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(3) == ("G" or "g"):
-                item_weight = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))/100
+                item_volume = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))/100
             else:
-                item_weight = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))
+                item_volume = float(re.search(r'((\d{1,3})\s*([Kk][Gg][Ss]?|[Gg]))', item_name).group(2))
             if re.search(r'(\d+)[x|X]', item_name):
-                item_weight = float(re.search(r'(\d+)[x|X]', item_name).group(1)) * item_weight
+                item_volume = float(re.search(r'(\d+)[x|X]', item_name).group(1)) * item_volume
 
         if item.select_one("div:nth-child(2) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1)"):
             item_link = item.select_one("div:nth-child(2) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1)")['href']
         
-        if (item_name and item_price and item_concentration and item_weight) and ((item_price/item_weight) < (cheaper_product['item_price']/cheaper_product['item_weight'])):
+        if (item_name and item_price and item_concentration and item_volume) and ((item_price/item_volume) < (cheaper_product['item_price']/cheaper_product['item_volume'])):
             cheaper_product['item_description'] = item_name
-            cheaper_product['item_weight'] = item_weight
+            cheaper_product['item_volume'] = item_volume
             cheaper_product['item_concentration'] = item_concentration
             cheaper_product['item_price'] = item_price
             cheaper_product['item_link'] = item_link
@@ -97,7 +97,7 @@ def get_cheaper_soda(request):
     if cheaper_americanas['item_description'] == '' and cheaper_ml['item_description'] == '':
         return Response(status=400)
     else:
-        if (cheaper_americanas['item_price']/cheaper_americanas['item_weight']) < (cheaper_ml['item_price']/cheaper_ml['item_weight']):
+        if (cheaper_americanas['item_price']/cheaper_americanas['item_volume']) < (cheaper_ml['item_price']/cheaper_ml['item_volume']):
             return Response(cheaper_americanas, status=200)
         else:
             return Response(cheaper_ml, status=200)
