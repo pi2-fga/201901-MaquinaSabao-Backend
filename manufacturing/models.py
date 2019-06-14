af527ee81e6982f46edca5bcc3e4f98a9f16fcad
Carlos Aragon <carloseraragon@gmail.com>
2019-06-13 21:51:31 -0300

    fixing soap price scraping layer

--- manufacturing/models.py
+++ manufacturing/models.py
@@ -7,15 +7,14 @@ from bi.views import *
 
 # Create your models here.
 class Manufacturing(models.Model):
-    date_of_manufacture = models.DateField()
-    start_of_manufacture = models.TimeField()
-    end_of_manufacture = models.TimeField()
+    start_of_manufacture = models.DateTimeField()
+    end_of_manufacture = models.DateTimeField()
     amount_of_soap = models.DecimalField(decimal_places=2, max_digits=4)
     expected_ph = models.DecimalField(decimal_places=2, max_digits=4)
     actual_ph = models.DecimalField(decimal_places=2, max_digits=4)
     oil_quality = models.CharField(max_length = 10)
     have_fragrance = models.BooleanField()
-    oil_image = models.ImageField(upload_to='static/oil_images/')
+    oil_image = models.ImageField(upload_to='static/oil_images/', blank=True)
     internet_soap_price = models.DecimalField(decimal_places=8, max_digits=12, blank=True)
     internet_soda_price = models.DecimalField(decimal_places=8, max_digits=12, blank=True)
     internet_alcohol_price = models.DecimalField(decimal_places=8, max_digits=12, blank=True)
@@ -36,7 +35,7 @@ class Manufacturing(models.Model):
             good_folder.write(image)
 
     def get_soap_price(self):
-        lojas_americanas_url = requests.get("https://www.americanas.com.br/busca/detergente-liquido")
+        lojas_americanas_url = requests.get("https://www.americanas.com.br/busca/desinfetantes-liquido")
 
         soup = bs(lojas_americanas_url.content, 'html.parser')
 
@@ -63,11 +62,20 @@ class Manufacturing(models.Model):
                 cheaper_product['item_price'] = item_price
         return (cheaper_product['item_price'] / cheaper_product['item_volume'])
 
+    def get_ammount_of_soda(self,soda_price):
+        if self.amount_of_soap == 2:
+            return (0.150 * soda_price)
+        if self.amount_of_soap == 4:
+            return (0.200 * soda_price)
+        if self.amount_of_soap == 8:
+            return (0.250 * soda_price)
+
+
     def save(self, *args, **kwargs):
         soda_price = requests.get('http://0.0.0.0:8000/get_cheaper_soda/').json()
         alcohol_price = requests.get('http://0.0.0.0:8000/get_cheaper_alcohol_ml/').json()
-        self.internet_soda_price = soda_price['item_price']/soda_price['item_volume']
-        self.internet_alcohol_price = alcohol_price['item_price']/alcohol_price['item_volume']
-        self.internet_soap_price = self.get_soap_price()
+        self.internet_soda_price = self.get_ammount_of_soda(soda_price['item_price']/soda_price['item_volume'])
+        self.internet_alcohol_price = (float(self.amount_of_soap) * 0.0625)*(alcohol_price['item_price']/alcohol_price['item_volume'])
+        self.internet_soap_price = (self.get_soap_price() * float(self.amount_of_soap) * 1000.00) 
         super(Manufacturing, self).save(*args, **kwargs)
         self.reallocate_image()
